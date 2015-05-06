9f3f274fa3ce5186982c9291c13f7593ce75d4af
Kat Huang <katavie@users.noreply.github.com>
2015-05-06 15:08:28 -0400

    Update cookies.js
    Add variables again

--- js/cookies.js
+++ js/cookies.js
@@ -14,11 +14,17 @@ function saveData() {
     Cookies.set("animal", formAnimal);
     Cookies.set("text", formText);
     
-    // sets form values to cookies
-    Cookies.set("name", document.getElementById("first-name").value);
-    Cookies.set("color", document.getElementById("color").value);
-    Cookies.set("animal", document.getElementById("animal").value);
-    Cookies.set("text", document.getElementById("text").value);
+    // sets form values to variables
+    var formName = document.getElementById("first-name").value;
+    var formColor = document.getElementById("color").value;
+    var formAnimal = document.getElementById("animal").value;
+    var formText = document.getElementById("text").value;
+
+    // sets variables from form values to cookies
+    Cookies.set("name", formName);
+    Cookies.set("color", formColor);
+    Cookies.set("animal", formAnimal);
+    Cookies.set("text", formText);
 
     var t0 = performance.now(); // begin timing
         // sets cookies to string variables of same name
